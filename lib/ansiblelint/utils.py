1a90df198ec1d31a14e88d144a7fed23e642ae32
Andreas Neumann <andreas.neumann@inverso.de>
2020-12-07 20:31:37 +0100

    QS: use build-in syntax for set()

--- lib/ansiblelint/utils.py
+++ lib/ansiblelint/utils.py
@@ -597,8 +597,7 @@ def get_action_tasks(yaml, file):
     tasks[:] = [task for task in tasks if all(k not in task for k in block_rescue_always)]
 
     return [task for task in tasks if
-            set(['include', 'include_tasks',
-                'import_playbook', 'import_tasks']).isdisjoint(task.keys())]
+            {'include', 'include_tasks', 'import_playbook', 'import_tasks'}.isdisjoint(task.keys())]
 
 
 def get_normalized_tasks(yaml, file):
